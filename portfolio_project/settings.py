f54b11d74a795c8f2c404c94ec5f0f03654974c6
Vinay Adusumilli <146542274+vinayadusumilli@users.noreply.github.com>
2025-12-09 22:38:07 +0000

    Add sumedhrajarshi.pythonanywhere.com to ALLOWED_HOSTS

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8eov!ej@&q!1_ss!h$a#uht-zg0orj%#75o2_zd=mhb5j*(ng0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sumedhrajarshi.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
